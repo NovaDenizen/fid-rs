571207a6d02e0af2207a4e9b65cb55669e836b56
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-09 03:29:56 +0000

    Deploy laysakura/fid-rs to github.com/laysakura/fid-rs.git:gh-pages

---
+++ aliases.js
@@ -0,0 +1,16 @@
+var ALIASES = {};
+ALIASES["arrayvec"] = {};
+ALIASES["cfg_if"] = {};
+ALIASES["crossbeam_deque"] = {};
+ALIASES["crossbeam_epoch"] = {};
+ALIASES["crossbeam_utils"] = {};
+ALIASES["either"] = {};
+ALIASES["fid_rs"] = {};
+ALIASES["lazy_static"] = {};
+ALIASES["libc"] = {};
+ALIASES["memoffset"] = {};
+ALIASES["nodrop"] = {};
+ALIASES["num_cpus"] = {};
+ALIASES["rayon"] = {};
+ALIASES["rayon_core"] = {};
+ALIASES["scopeguard"] = {};
